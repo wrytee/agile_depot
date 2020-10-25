a56fed37316b1b7dbccf396c27cb0edff2f70227
Matt Wright <38134138+wrytee@users.noreply.github.com>
2020-10-25 01:32:52 +0000

    improve cart security

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_003703) do
+ActiveRecord::Schema.define(version: 2020_10_25_004826) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
